6704cda77c8e67321df6f1fe798243d1e8d4ea85
Avibun <dadninele@gmail.com>
2025-10-21 14:16:13 +0800

    Add counter functionality and history tracking to the widget
    - Implemented interactivity callbacks for counter actions in main.dart.
    - Enhanced CounterWidget with buttons for incrementing and decrementing the counter.
    - Added history tracking for counter values, saving to widget data.
    - Updated AndroidManifest.xml to include necessary receivers and services.
    - Modified build.gradle to include Material3 dependency.
    - Adjusted configuration.xml for proper widget layout.

--- android/app/src/main/kotlin/com/example/acc/CounterWidget.kt
+++ android/app/src/main/kotlin/com/example/acc/CounterWidget.kt
@@ -2,29 +2,36 @@ package com.example.acc
 
 import HomeWidgetGlanceState
 import HomeWidgetGlanceStateDefinition
+import androidx.core.content.edit
 import android.content.Context
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.glance.Button
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
+import androidx.glance.action.ActionParameters
 import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.action.ActionCallback
+import androidx.glance.appwidget.action.actionRunCallback
 import androidx.glance.appwidget.provideContent
 import androidx.glance.background
-import androidx.glance.currentState
-import androidx.glance.layout.Box
-import androidx.glance.layout.Column
+import androidx.glance.layout.*
 import androidx.glance.state.GlanceStateDefinition
 import androidx.glance.text.Text
+import androidx.glance.currentState
+import android.os.Handler
+import android.os.Looper
+import androidx.core.net.toUri
+import es.antonborri.home_widget.HomeWidgetBackgroundIntent
+import es.antonborri.home_widget.HomeWidgetPlugin
 
 class CounterWidget: GlanceAppWidget() {
 
-    override val stateDefinition : GlanceStateDefinition<*>?
-        get() = HomeWidgetGlanceStateDefinition()
+   override val stateDefinition : GlanceStateDefinition<*>?
+       get() = HomeWidgetGlanceStateDefinition()
 
-    override suspend fun provideGlance(
-        context: Context,
-        id: GlanceId
-    ) {
+    override suspend fun provideGlance(context: Context, id: GlanceId) {
         provideContent {
             GlanceContent(context, currentState())
         }
@@ -32,14 +39,169 @@ class CounterWidget: GlanceAppWidget() {
 
     @Composable
     private fun GlanceContent(context: Context, currentState: HomeWidgetGlanceState) {
-        val prefs = currentState.preferences
-        val counter = prefs.getInt("counter", 0)
-
-        Box(modifier = GlanceModifier.background(Color.White)) {
-            Column {
-                Text(text = "Counter: ${counter.toString()}")
+        val counter = currentState.preferences.getInt("counter", 0)
+        Box(modifier = GlanceModifier.background(Color.White).fillMaxSize()) {
+            Column (
+                modifier = GlanceModifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                    .padding(8.dp)
+                    .padding(top = 16.dp),
+                verticalAlignment = Alignment.Vertical.CenterVertically,
+                horizontalAlignment = Alignment.Horizontal.CenterHorizontally
+            ){
+                Text(
+                    text = counter.toString()
+                )
+                Row (
+                    modifier = GlanceModifier
+                        .fillMaxWidth()
+                        .fillMaxHeight(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Box (
+                        modifier = GlanceModifier
+                            .fillMaxWidth()
+                            .fillMaxHeight()
+                            .defaultWeight()
+                            .padding(8.dp),
+                    ){
+                        Button(
+                            text = "4",
+                            onClick = actionRunCallback<SubOneActionCallBack>(),
+                            modifier = GlanceModifier
+                                .fillMaxWidth()
+                                .fillMaxHeight()
+                        )
+                    }
+                    Box (
+                        modifier = GlanceModifier
+                            .fillMaxWidth()
+                            .fillMaxHeight()
+                            .defaultWeight()
+                            .padding(8.dp),
+                    ){
+                        Button(
+                            text = "3",
+                            onClick = actionRunCallback<AddHundActionCallBack>(),
+                            modifier = GlanceModifier
+                                .fillMaxWidth()
+                                .fillMaxHeight()
+                        )
+                    }
+                    Box (
+                        modifier = GlanceModifier
+                            .fillMaxWidth()
+                            .fillMaxHeight()
+                            .defaultWeight()
+                            .padding(8.dp),
+                    ){
+                        Button(
+                            text = "2",
+                            onClick = actionRunCallback<AddTenActionCallBack>(),
+                            modifier = GlanceModifier
+                                .fillMaxWidth()
+                                .fillMaxHeight()
+                        )
+                    }
+                    Box (
+                        modifier = GlanceModifier
+                            .fillMaxWidth()
+                            .fillMaxHeight()
+                            .defaultWeight()
+                            .padding(8.dp),
+                    ){
+                        Button(
+                            text = "1",
+                            onClick = actionRunCallback<AddOneActionCallBack>(),
+                            modifier = GlanceModifier
+                                .fillMaxWidth()
+                                .fillMaxHeight()
+                        )
+                    }
+                }
             }
         }
     }
 
+    companion object {
+        private var timerHandler: Handler? = null
+        private var timerRunnable: Runnable? = null
+
+        fun resetTimer(context: Context, GlancId: GlanceId) {
+            timerHandler?.removeCallbacks(timerRunnable!!)
+
+            val backgroundIntent = HomeWidgetBackgroundIntent.getBroadcast(
+                context,
+                "counter://timeout".toUri()
+            )
+
+            timerRunnable = Runnable {
+                backgroundIntent.send()
+            }
+
+            timerHandler = Handler(Looper.getMainLooper())
+            timerHandler?.postDelayed(timerRunnable!!, 2000)
+        }
+    }
 }
+
+class SubOneActionCallBack : ActionCallback {
+    override suspend fun onAction(
+        context: Context,
+        glanceId: GlanceId,
+        parameters: ActionParameters
+    ) {
+        val prefs = context.getSharedPreferences("HomeWidgetPreferences", Context.MODE_PRIVATE)
+        val currentCount = prefs.getInt("counter", 0)
+        prefs.edit(commit = true) { putInt("counter", currentCount - 1) }
+
+        CounterWidget().update(context, glanceId)
+        CounterWidget.resetTimer(context, glanceId)
+    }
+}
+class AddHundActionCallBack : ActionCallback {
+    override suspend fun onAction(
+        context: Context,
+        glanceId: GlanceId,
+        parameters: ActionParameters
+    ) {
+        val prefs = context.getSharedPreferences("HomeWidgetPreferences", Context.MODE_PRIVATE)
+        val currentCount = prefs.getInt("counter", 0)
+        prefs.edit(commit = true) { putInt("counter", currentCount + 100) }
+
+        CounterWidget().update(context, glanceId)
+        CounterWidget.resetTimer(context, glanceId)
+    }
+}
+class AddTenActionCallBack : ActionCallback {
+    override suspend fun onAction(
+        context: Context,
+        glanceId: GlanceId,
+        parameters: ActionParameters
+    ) {
+        val prefs = context.getSharedPreferences("HomeWidgetPreferences", Context.MODE_PRIVATE)
+        val currentCount = prefs.getInt("counter", 0)
+        prefs.edit(commit = true) { putInt("counter", currentCount + 10) }
+
+        CounterWidget().update(context, glanceId)
+        CounterWidget.resetTimer(context, glanceId)
+    }
+}
+class AddOneActionCallBack : ActionCallback {
+    override suspend fun onAction(
+        context: Context,
+        glanceId: GlanceId,
+        parameters: ActionParameters
+    ) {
+        val prefs = context.getSharedPreferences("HomeWidgetPreferences", Context.MODE_PRIVATE)
+        val currentCount = prefs.getInt("counter", 0)
+        prefs.edit(commit = true) { putInt("counter", currentCount + 1) }
+
+        CounterWidget().update(context, glanceId)
+        CounterWidget.resetTimer(context, glanceId)
+    }
+}
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,33 @@ import 'package:home_widget/home_widget.dart';
 
 void main() {
   runApp(const MyApp());
+  HomeWidget.registerInteractivityCallback(counterCallBack);
+}
+
+@pragma('vm:entry-point')
+Future<void> counterCallBack(Uri? uri) async {
+  print('CounterCallBack invoked: $uri');
+  if (uri?.scheme == "counter" && uri?.host == "timeout") {
+    final int counterValue =
+        await HomeWidget.getWidgetData('counter', defaultValue: 0) as int;
+    print('Current counter value: $counterValue');
+
+    // TODO : save to supabase
+    final String? historyJson =
+        await HomeWidget.getWidgetData<String>('counter_history');
+    List<int> history = [];
+    print('Current history json: $historyJson');
+    if (historyJson != null && historyJson.isNotEmpty) {
+      history =
+          (historyJson.split(',').map((e) => int.tryParse(e) ?? 0).toList());
+    }
+    history.add(counterValue);
+    await HomeWidget.saveWidgetData<String>(
+        'counter_history', history.join(','));
+
+    await HomeWidget.saveWidgetData<int>('counter', 0);
+    await HomeWidget.updateWidget(name: 'CounterWidgetReceiver');
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -32,14 +59,38 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  List<int> _counterHistory = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
+  Future<void> _loadData() async {
+    final int counterValue =
+        await HomeWidget.getWidgetData('counter', defaultValue: 0) as int;
+    final String? historyJson =
+        await HomeWidget.getWidgetData<String>('counter_history');
+
+    List<int> history = [];
+    if (historyJson != null && historyJson.isNotEmpty) {
+      history =
+          historyJson.split(',').map((e) => int.tryParse(e) ?? 0).toList();
+    }
+
+    setState(() {
+      _counter = counterValue;
+      _counterHistory = history;
+    });
+  }
 
   Future<void> _incrementCounter() async {
-    final int counterValue = await HomeWidget.getWidgetData('counter', defaultValue: 0) as int;
+    final int counterValue =
+        await HomeWidget.getWidgetData('counter', defaultValue: 0) as int;
     final int newValue = counterValue + 1;
     await HomeWidget.saveWidgetData<int>('counter', newValue);
-    await HomeWidget.updateWidget(
-      name: 'AccWidget'
-    );
+    await HomeWidget.updateWidget(name: 'CounterWidgetReceiver');
     setState(() {
       _counter = newValue;
     });
@@ -49,9 +100,15 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.refresh),
+              onPressed: _loadData,
+              tooltip: 'Refresh History',
+            )
+          ]),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -63,6 +120,31 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            const SizedBox(height: 40),
+            if (_counterHistory.isNotEmpty) ...[
+              const Divider(),
+              const Text(
+                'Accounting History:',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 10),
+              Expanded(
+                  child: ListView.builder(
+                itemCount: _counterHistory.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    margin:
+                        const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+                    child: ListTile(
+                        leading: CircleAvatar(
+                          child: Text('${index + 1}'),
+                        ),
+                        title: Text('Gold: ${_counterHistory[index]}'),
+                        subtitle: Text('HAHAHAHAHAHAH')),
+                  );
+                },
+              ))
+            ]
           ],
         ),
       ),
